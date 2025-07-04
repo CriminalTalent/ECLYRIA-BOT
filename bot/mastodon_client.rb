e33b20856cd5cce99e213e07214f6ceeee73f42f
CriminalTalent <rntjdrud@naver.com>
2025-07-04 22:45:55 +0900

    Update mastodon_client.rb

--- bot/mastodon_client.rb
+++ bot/mastodon_client.rb
@@ -1,15 +1,26 @@
 # bot/mastodon_client.rb
 require 'mastodon'
+require 'dotenv'
+Dotenv.load(File.expand_path('../.env', __dir__))
+
+puts "🧪 base_url: #{ENV['MASTODON_BASE_URL']}"
+puts "🧪 token: #{ENV['MASTODON_TOKEN'][0..10]}..." if ENV['MASTODON_TOKEN']
 
 module MastodonClient
-  @client = Mastodon::REST::Client.new(
-    base_url: ENV['MASTODON_BASE_URL'],
-    bearer_token: ENV['MASTODON_TOKEN']
-  )
+  def self.client
+    @client ||= Mastodon::REST::Client.new(
+      base_url: ENV['MASTODON_BASE_URL'],
+      bearer_token: ENV['MASTODON_TOKEN']
+    )
+  end
 
   def self.listen_mentions
-    # 예시: mentions 받아오기
-    mentions = @client.notifications.select { |n| n.type == 'mention' }
+    mentions = client.notifications.select { |n| n.type == 'mention' }
     mentions.each { |mention| yield mention }
+  end
+
+  def self.reply(mention, message)
+    acct = mention.account.acct
+    client.create_status("@#{acct} #{message}", in_reply_to_id: mention.status.id)
   end
 end
