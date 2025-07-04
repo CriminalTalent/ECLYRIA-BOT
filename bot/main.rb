7d05b6befad8652535fcb0735ef6c9dbe2b967d0
CriminalTalent <rntjdrud@naver.com>
2025-07-04 22:43:52 +0900

    Update main.rb

--- bot/main.rb
+++ bot/main.rb
@@ -1,9 +1,12 @@
-# bot/main.rb
-require 'dotenv/load'
+require 'dotenv'
+Dotenv.load(File.expand_path('../.env', __dir__))
+
 require_relative 'mastodon_client'
 require_relative 'command_parser'
 
-puts " ECLYRIA봇 기동 완료!"
+puts "✅ ECLYRIA봇 기동 완료!"
+puts "📡 BASE_URL: #{ENV['MASTODON_BASE_URL']}"
+puts "🔐 TOKEN 시작: #{ENV['MASTODON_TOKEN'][0..10]}..." if ENV['MASTODON_TOKEN']
 
 loop do
   MastodonClient.listen_mentions do |mention|
