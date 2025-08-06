8f09bac6896546fca461bf3eb6020595253f46b0
CriminalTalent <rntjdrud@naver.com>
2025-08-06 21:51:29 +0900

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,15 +1,31 @@
+# main.rb
+require 'dotenv/load'
+require 'google_drive'
 require_relative 'mastodon_client'
 require_relative 'command_parser'
 require_relative 'sheet_manager'
-require 'google_drive'
-require 'dotenv/load'
 
-session = GoogleDrive::Session.from_config("config.json")
-spreadsheet = session.spreadsheet_by_key(ENV["GOOGLE_SHEET_ID"])
+# Google Sheets 인증 및 세션
+unless MastodonClient.validate_environment
+  puts "환경 설정이 올바르지 않습니다. .env 파일을 확인해주세요."
+  exit
+end
+
+begin
+  session = GoogleDrive::Session.from_config("config.json")
+  spreadsheet = session.spreadsheet_by_key(ENV["GOOGLE_SHEET_ID"])
+rescue => e
+  puts "Google Sheets 연결 실패: #{e.message}"
+  exit
+end
+
+# 시트 관리자 객체
 sheet_manager = SheetManager.new(spreadsheet)
 
+# 마스토돈 클라이언트
 client = MastodonClient.client
 
+# 스트리밍 시작
 client.stream_user do |event|
   if event.is_a?(Mastodon::Notification) && event.type == 'mention'
     CommandParser.parse(client, sheet_manager, event)
