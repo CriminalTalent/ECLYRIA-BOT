8fa093b846408e62cf0f92cc7f066141d3443808
CriminalTalent <rntjdrud@naver.com>
2025-08-06 21:22:51 +0900

    Update buy_command.rb

--- commands/buy_command.rb
+++ commands/buy_command.rb
@@ -8,10 +8,10 @@ class BuyCommand
 
   def execute
     player = @sheet.get_player(@student_id)
-    return "학적부에 학생이 없는데? 교수님에게 가보렴." unless player
+    return "등록된 플레이어가 아니란다. 먼저 등록해줘야 해!" unless player
 
-    if player["debt"].to_i > 0
-      return "빚이 있으면 상점에서 물건을 살 수 없단다. 먼저 갚고 오렴."
+    if player["galleons"].to_i < 0
+      return "갈레온이 마이너스 상태라 구매는 불가능하단다… 먼저 빚을 갚고 오렴!"
     end
 
     item = @sheet.get_item(@item_name)
@@ -21,17 +21,14 @@ class BuyCommand
     galleons = player["galleons"].to_i
 
     if galleons < price
-      return "갈레온이 부족하단다. 보유 갈레온 #{galleons}"
+      return "갈레온이 부족하단다. 가진 돈: #{galleons}, 필요한 돈: #{price}"
     end
 
-       # 구매 처리
+    # 갈레온 차감 및 아이템 추가
     player["galleons"] = galleons - price
     inventory = player["items"].to_s.split(",").map(&:strip)
     inventory << @item_name
     player["items"] = inventory.join(",")
-
-    # 시트에 반영
-    @sheet.update_player(@student_id, player)
 
     return "#{@item_name}을(를) #{price}갈레온에 구매했단다. 현재 잔액: #{player["galleons"]}갈레온"
   end
