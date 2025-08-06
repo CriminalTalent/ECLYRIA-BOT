c6fba03115a34a4ed237d2a53d9f8d0470798f57
CriminalTalent <rntjdrud@naver.com>
2025-08-06 21:25:13 +0900

    Update coin_command.rb

--- commands/coin_command.rb
+++ commands/coin_command.rb
@@ -1,1 +1,12 @@
 
+# commands/coin_command.rb
+class CoinCommand
+  def initialize(student_id)
+    @student_id = student_id
+  end
+
+  def execute
+    result = ["앞면", "뒷면"].sample
+    return "#{result}"
+  end
+end
