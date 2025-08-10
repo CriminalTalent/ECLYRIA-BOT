8c33c0ab238dd8569d8a2ac3e200e3ae0c196975
CriminalTalent <rntjdrud@naver.com>
2025-08-10 21:38:15 +0900

    Update tarot_command.rb

--- commands/tarot_command.rb
+++ commands/tarot_command.rb
@@ -38,7 +38,8 @@ class TarotCommand
     puts "[DEBUG] 타로 카드 뽑기 완료: #{@student_id} - #{card}"
 
     return <<~TEXT.strip
- 
+      신비로운 운세
+      
       [#{card}]
       #{meaning}
       
@@ -46,6 +47,7 @@ class TarotCommand
       행운의 색: #{lucky_color}
       행운의 장소: #{lucky_place}
       
+      언제든 다시 뽑아도 된단다~
     TEXT
   end
 end
