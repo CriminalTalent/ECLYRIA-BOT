f7f2ff29a25368e4d53503139f6b3f08ca3b540b
CriminalTalent <rntjdrud@naver.com>
2025-10-16 22:57:58 +0900

    Update dice_command.rb

--- commands/dice_command.rb
+++ commands/dice_command.rb
@@ -1,16 +1,13 @@
+# ============================================
 # commands/dice_command.rb
+# ============================================
 class DiceCommand
-  def initialize(student_id, max)
-    @student_id = student_id
-    @max = max.to_i
+  def initialize(student_id, sides)
+    @student_id = student_id.gsub('@', '')
+    @sides = sides
   end
 
   def execute
-    if @max < 1 || @max > 100
-      return "주사위는 1 이상 100 이하 숫자까지만 가능하단다~"
-    end
-
-    result = rand(1..@max)
-    return "#{result}"
+    return "#{rand(1..@sides)}"
   end
 end
