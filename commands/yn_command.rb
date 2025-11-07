7e663c81debf900ca2473339ab8c017c2fa377f4
CriminalTalent <rntjdrud@naver.com>
2025-11-07 21:06:40 +0900

    Update yn_command.rb

--- commands/yn_command.rb
+++ commands/yn_command.rb
@@ -1,12 +1,14 @@
 # ============================================
 # commands/yn_command.rb
 # ============================================
+# encoding: UTF-8
 class YnCommand
   def initialize(student_id)
     @student_id = student_id.gsub('@', '')
   end
 
   def execute
+    # 단답형 응답: YES / NO / Maybe / Why not?
     return ["YES", "NO", "Maybe", "Why not?"].sample
   end
 end
