08ba41bdb25030ef45eb4660edb355924d27e3a7
CriminalTalent <rntjdrud@naver.com>
2025-11-08 22:20:39 +0900

    Update coin_command.rb

--- commands/coin_command.rb
+++ commands/coin_command.rb
@@ -3,11 +3,7 @@
 # ============================================
 # encoding: UTF-8
 class CoinCommand
-  def initialize(student_id)
-    @student_id = student_id.gsub('@', '')
-  end
-
-  def execute
-    return ["앞면", "뒷면"].sample
+  def self.run(_mastodon_client, _notification)
+    ["앞면", "뒷면"].sample
   end
 end
